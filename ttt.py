efb158992610e02bcc94692d6f5b06e83d0dee70
Dale <dale.a.lenze@gmail.com>
2025-04-08 21:08:06 -0500

    Added main() function and if name is main block.
    Also changed the format of the new_board function.

--- ttt.py
+++ ttt.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 def new_board():
-    board = [[None, None, None], 
-             [None, None, None], 
-             [None, None, None]]
+    global board
+    board = [[None, None, None],[None, None, None],[None, None, None],]
     return board
 
 def render_board(board):
     for item in board:
         print(item)
-    
-print(render_board(new_board()))
+
+def main():
+    board = []
+    print(render_board(new_board()))
+
+if __name__ == "__main__":
+    main()
