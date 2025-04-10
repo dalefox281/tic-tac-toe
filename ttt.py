a5ececff632659bb2e59b9e851e648281f76cd24
Dale <dale.a.lenze@gmail.com>
2025-04-10 15:31:15 -0500

    changed some code and added a test board for debug
    Also imported pprint to see how I can use that to
    make the game a little more easy on the eyes.

--- ttt.py
+++ ttt.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
-def new_board():
-    global board
-    board = [[None, None, None],[None, None, None],[None, None, None],]
+import pprint
+
+def main():
+    # testBoard = [['X','X','X'],[None, None, None],[None, None, None]]
+    board = []
+    game = True
+    render_board(new_board(board))
+    while game == True:
+        render_board(board)
+
+def new_board(board):
+    board = [[None, None, None],[None, None, None],[None, None, None]]
     return board
 
 def render_board(board):
     for item in board:
-        print(item)
-
-def main():
-    board = []
-    print(render_board(new_board()))
+        pprint.pprint(item)
+        
 
 if __name__ == "__main__":
+    board = []
     main()
